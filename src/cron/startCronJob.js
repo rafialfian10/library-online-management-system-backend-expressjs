16332c19c87577d673ea860714b63943f915c7d6
rafialfian10 <rafialfian770@gmail.com>
2024-06-19 10:46:10 +0700

    feat(cron): add function create fine with cron library

--- src/cron/startCronJob.js
+++ src/cron/startCronJob.js
@@ -1,5 +1,4 @@
 const cron = require("node-cron");
-
 const { Transactions } = require("../../database/models");
 const { createFine } = require("../repositories/fineRepository");
 
@@ -12,29 +11,32 @@ const startCronJob = () => {
       const overdueTransactions = await Transactions.findAll({
         where: {
           isStatus: true,
+          transactionType: "Borrow"
         },
       });
 
-      for (const transaction of overdueTransactions) {
-        const returnDate = new Date(transaction.returnDate);
-        let secondsRemaining = Math.ceil((returnDate - currentDate) / 1000);
+      if (overdueTransactions && overdueTransactions.length > 0) {
+        for (const transaction of overdueTransactions) {
+          const returnDate = new Date(transaction.returnDate);
+          let secondsRemaining = Math.ceil((returnDate - currentDate) / 1000);
+          
+          if (secondsRemaining === -0) {
+            secondsRemaining = 0;
+          }
 
-        if (secondsRemaining === -0) {
-          secondsRemaining = 0;
-        }
+          if (secondsRemaining === 0) {
+            const fine = {
+              idBook: transaction.idBook,
+              idUser: transaction.idUser,
+              totalDay: 1,
+              totalFine: process.env.FINE_PER_DAY * transaction.totalBook,
+              status: null,
+              token: null,
+            };
 
-        if (secondsRemaining === 0) {
-          const fine = {
-            idBook: transaction.idBook,
-            idUser: transaction.idUser,
-            totalDay: 1,
-            totalFine: process.env.FINE_PER_DAY * transaction.totalBook,
-            status: null,
-            token: null,
-          };
-
-          await createFine(fine);
-          // console.log("Fine Created for Transaction ID:", transaction.id);
+            await createFine(fine);
+            // console.log("Fine Created for Transaction ID:", transaction.id);
+          }
         }
       }
     } catch (error) {
